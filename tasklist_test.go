95042fa429821bd9a8033f958f589de48f2246c1
Geoffrey Grosenbach <boss@topfunky.com>
2018-01-28 14:39:52 -0800

    Refactor test to individual methods

--- tasklist_test.go
+++ tasklist_test.go
@@ -4,20 +4,32 @@ import (
 	"testing"
 )
 
-func TestTaskListFromFilename(t *testing.T) {
+func makeTaskList() TL {
 	lines := []string{
 		"- [x] BOD 08:00",
 		"- [ ] Make bacon 09:00",
 		"- [ ] Reply to emails 09:30",
 		"- [ ] Call Krusty the Clown 10:00",
 	}
-	taskList := TaskList("# Monday", lines)
+	return TaskList("# Monday", lines)
+}
+
+func TestTaskListMakesTasks(t *testing.T) {
+	taskList := makeTaskList()
 	if len(taskList.tasks) != 4 {
 		t.Error("Expected: 4, Got:", len(taskList.tasks))
 	}
+}
+
+func TestTaskListCalculatesDurationOfFirstTask(t *testing.T) {
+	taskList := makeTaskList()
 	if taskList.tasks[0].duration != 0 {
 		t.Error("Expected BOD task to have duration 0 but it was", taskList.tasks[0].duration)
 	}
+}
+
+func TestTaskListCalculatesDurationOfOtherTask(t *testing.T) {
+	taskList := makeTaskList()
 	if taskList.tasks[1].duration != 60 {
 		t.Error("Expected 'Make bacon' task to have duration 60 but it was", taskList.tasks[1].duration)
 	}
