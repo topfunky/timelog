fd5776db6938e9daa9f26bed0b61f4328a925a36
Geoffrey Grosenbach <boss@topfunky.com>
2018-01-28 14:52:28 -0800

    Refactor to task constructor in tests

--- task_test.go
+++ task_test.go
@@ -4,8 +4,16 @@ import (
 	"testing"
 )
 
+func makeUncompletedTask() T {
+	return Task("- [ ] Make bacon 09:34")
+}
+
+func makeCompletedTask() T {
+	return Task("- [x] Make bacon 09:35")
+}
+
 func TestTaskUncompleted(t *testing.T) {
-	task := Task("- [ ] Make bacon 09:34")
+	task := makeUncompletedTask()
 	if task.description != "Make bacon" {
 		t.Error("Expected: Make bacon, Got:", task.description)
 	}
@@ -15,9 +23,9 @@ func TestTaskUncompleted(t *testing.T) {
 }
 
 func TestTaskCompleted(t *testing.T) {
-	task := Task("- [x] Make bacon 09:35")
+	task := makeCompletedTask()
 	if task.description != "Make bacon" {
-		t.Error("Expected: Make bacon", task.description)
+		t.Error("Expected: 'Make bacon', Got:", task.description)
 	}
 	if task.timestamp != 575 {
 		t.Error("Expected: 575, Got:", task.timestamp)
