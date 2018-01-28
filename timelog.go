bc225ac835bca1d124f003ca93608ff0e491a5c3
Geoffrey Grosenbach <boss@topfunky.com>
2018-01-27 22:44:42 -0800

    Refactor to proper test and implementation files

--- timelog.go
+++ timelog.go
@@ -1,26 +1,52 @@
 package main
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
-	"github.com/briandowns/spinner"
 	"github.com/gosuri/uiprogress"
 )
+
+// T is the data structure for a single task
+type T struct {
+	raw         string
+	minutes     int
+	description string
+}
+
+// Task creates a new task consisting of a description and a number of minutes
+func Task(raw string) (t T) {
+	t.raw = raw
+	t.description = raw
+	t.minutes = 0
+
+	t.description = strings.TrimPrefix(t.description, "- [ ] ")
+	t.description = strings.TrimPrefix(t.description, "- [x] ")
+	if match, _ := regexp.MatchString(" ([0-9][0-9]:[0-9][0-9])$", t.description); match {
+		minute, err := strconv.Atoi(t.description[len(t.description)-2:])
+		if err != nil {
+			fmt.Println(minute)
+		}
+		hour, err := strconv.Atoi(t.description[len(t.description)-5 : len(t.description)-3])
+		if err != nil {
+			fmt.Println(err)
+		}
+		t.minutes = hour*60 + minute
+		t.description = t.description[:len(t.description)-6]
+	}
+	return
+}
 
 func main() {
 	doProgress()
 	// doSpinner()
 }
 
-func doSpinner() {
-	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond) // Build our new spinner
-	s.Start()                                                    // Start the spinner
-	time.Sleep(10 * time.Second)                                 // Run for some time to simulate work
-	s.Stop()
-}
-
 func doProgress() {
-	duration := (25 * 60) / 15
+	duration := (45 * 60) / 15
 	uiprogress.Start()                 // start rendering
 	bar := uiprogress.AddBar(duration) // Add a new bar
 
@@ -28,7 +54,7 @@ func doProgress() {
 	bar.AppendElapsed()
 
 	bar.PrependFunc(func(b *uiprogress.Bar) string {
-		return "app: Bacon"
+		return "Task"
 	})
 
 	for bar.Incr() {
